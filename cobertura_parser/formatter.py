7cf2ddab9da78346a1e0924a976f768af7f5485a
Adam Gartee <agartee@gmail.com>
2023-03-17 11:26:49 -0400

    refactored formatter

--- cobertura_parser/formatter.py
+++ cobertura_parser/formatter.py
@@ -6,21 +6,42 @@ from cobertura_parser.coverage_item import CoverageItem
 def format_coverage_items(coverage_items: Iterator[CoverageItem]):
     class_name_width = max(len(ci.name) for ci in coverage_items)
     
-    header_names = ["Class Name", "Lines Coverable", "Lines Covered", "Branches", "Branches Covered"]
+    header_names = [
+        "Class Name", 
+        "Lines Coverable", 
+        "Lines Covered", 
+        "Branches", 
+        "Branches Covered"
+    ]
+
     header_lengths = [len(name) for name in header_names]
-    header_format = f"{{:<{class_name_width}}}  |  {{:>{header_lengths[1]}}}  |  {{:>{header_lengths[2]}}}  |  {{:>{header_lengths[3]}}}  |  {{:>{header_lengths[4]}}}"
-    row_format = f"{{:<{class_name_width}}}  |  {{:>{header_lengths[1]}}}  |  {{:>{header_lengths[2]}}}  |  {{:>{header_lengths[3]}}}  |  {{:>{header_lengths[4]}}}"
-    
-    separator_char = '-'
-    separator = f"{separator_char * (class_name_width+2)}|{separator_char * (header_lengths[1]+4)}|{separator_char * (header_lengths[2]+4)}|{separator_char * (header_lengths[3]+4)}|{separator_char * (header_lengths[4]+4)}"
-    
-    result = f"{separator}\n"
-    result += f"{header_format.format(*header_names)}\n"
-    result += f"{separator}\n"
+    header_lengths[0] = class_name_width
+
+    header_formats = [f"{{:<{header_lengths[i]}}}" 
+                      if i == 0 else f"{{:>{header_lengths[i]}}}" 
+                      for i in range(len(header_names))]
+    row_format = "  |  ".join(header_formats)
+
+    separator_row_parts = [f"{'-' * (header_lengths[0] + 2)}"]
+    separator_row_parts.extend(
+        [f"{'-' * (length + 4)}" for length in header_lengths[1:]])
+    separator_row = '|'.join(separator_row_parts)
+
+    result = f"{separator_row}\n"
+    result += f"{row_format.format(*header_names)}\n"
+    result += f"{separator_row}\n"
 
     for ci in coverage_items:
-        result += f"{row_format.format(ci.name, ci.coverable_lines, ci.covered_lines, ci.branches, ci.covered_branches)}\n"
+        ordered_column_values = [
+            ci.name, 
+            ci.coverable_lines, 
+            ci.covered_lines, 
+            ci.branches, 
+            ci.covered_branches
+        ]
+        
+        result += f"{row_format.format(*ordered_column_values)}\n"
     
-    result += f"{separator}\n"
+    result += f"{separator_row}\n"
 
     return result
