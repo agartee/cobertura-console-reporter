08afcd830382479b9bc9f09372a5033d3913dacc
Adam Gartee <agartee@gmail.com>
2023-03-20 10:28:24 -0400

    fixed bad copy/paste messages in test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -15,12 +15,12 @@ TESTS_DIR="$ROOT_DIR/tests"
 coverage run -m pytest "$TESTS_DIR" -v --no-header --capture=no
 
 echo
-echo -e "${BLUE}Test Coverage${NC}" 
+echo -e "${BLUE}Test Coverage:${NC}" 
 echo
 
 coverage report -m
 
 echo
-echo -e "${BLUE}Test Coverage${NC}" 
+echo -e "${BLUE}Linting:${NC}" 
 
 pylint "$ROOT_DIR/cobertura_console_reporter"
