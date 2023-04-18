03a7408ca2b6da0e076fbb58924f8b72cbc1b2de
Adam Gartee <agartee@gmail.com>
2023-04-18 13:23:05 -0400

    refactor/cleanup

--- cobertura_console_reporter/formatter.py
+++ cobertura_console_reporter/formatter.py
@@ -21,90 +21,128 @@ def format_coverage_items(
     Returns:
         str: Formatted string intended for console output.
     """
-    class_name_width = max(
-        [len(ci.class_name) for ci in coverage_items]
-        + [len(ci.class_namespace) for ci in coverage_items]
-    )
+    reset_color_val = Style.RESET_ALL if colorize is True else ""
+    class_name_length = _calc_class_name_length(coverage_items)
 
     header_names = [
         "Class Name",
-        f"% Lines",
-        f"% Branches",
+        "% Lines",
+        "% Branches",
         "Uncovered Line #s",
     ]
-
     header_lengths = [len(name) for name in header_names]
-    header_lengths[0] = max([class_name_width, len(header_names[0])])
-
-    reset_color = Style.RESET_ALL if colorize is True else ""
-
-    header_formats = [
-        f"{{:<{header_lengths[i]}}}" if i == 0 else f"{{:>{header_lengths[i]}}}"
-        for i in range(len(header_names))
-    ]
-    header_format = "  |  ".join(header_formats)
+    header_lengths[0] = max([class_name_length, len(header_names[0])])
+    header_format = "  |  ".join(
+        _build_header_row_formats(header_names, header_lengths)
+    )
 
     class_name_col_idx = 0
     uncovered_line_numbers_col_idx = 3
-    row_formats = [
-        f"{{color}}{{:<{header_lengths[i]}}}{reset_color}"
-        if i == class_name_col_idx or i == uncovered_line_numbers_col_idx
-        else f"{{color}}{{:>{header_lengths[i]}}}{reset_color}"
-        for i in range(len(header_names))
-    ]
+    row_formats = _build_data_row_format(
+        reset_color_val,
+        header_names,
+        header_lengths,
+        class_name_col_idx,
+        uncovered_line_numbers_col_idx,
+    )
     row_format = "  |  ".join(row_formats)
 
-    separator_row_parts = [f"{'-' * (header_lengths[0] + 2)}"]
-    separator_row_parts.extend(
-        [f"{'-' * (length + 4)}" for length in header_lengths[1:]]
-    )
-    separator_row = "|".join(separator_row_parts)
+    separator_row = _build_separator_row(header_lengths)
 
     result = f"{separator_row}\n"
     result += f"{header_format.format(*header_names)}\n"
     result += f"{separator_row}\n"
 
-    sorted_items = sorted(coverage_items, key=lambda x: x.class_namespace)
-    grouped_items = itertools.groupby(sorted_items, key=lambda x: x.class_namespace)
+    grouped_items = itertools.groupby(
+        sorted(coverage_items, key=lambda x: x.class_namespace),
+        key=lambda x: x.class_namespace,
+    )
 
     for key, group in grouped_items:
         items = list(group)
 
         if key != "":
-            ordered_group_column_values = [
-                key,
-                _format_percent(
-                    sum(item.covered_lines for item in items),
-                    sum(item.coverable_lines for item in items),
-                ),
-                _format_percent(
-                    sum(item.covered_branches for item in items),
-                    sum(item.branches for item in items),
-                ),
-                "",
-            ]
-
-            color = Fore.GREEN if colorize is True else ""
-            result += (
-                f"{row_format.format(color=color, *ordered_group_column_values)}\n"
-            )
+            result += _build_namespace_data_row(key, items, row_format, colorize)
 
         for item in items:
-            indent = "  " if key != "" else ""
-
-            ordered_column_values = [
-                indent + item.class_name,
-                _format_percent(item.covered_lines, item.coverable_lines),
-                _format_percent(item.covered_branches, item.branches),
-                _compact_number_ranges(item.uncovered_line_numbers),
-            ]
-
-            color = Fore.GREEN if colorize is True else ""
-            result += f"{row_format.format(color=color, *ordered_column_values)}\n"
+            result += _build_data_row(key, item, row_format, colorize)
 
     result += f"{separator_row}\n"
 
     return result
+
+
+def _build_separator_row(header_lengths: list[int]) -> str:
+    separator_row_parts = [f"{'-' * (header_lengths[0] + 2)}"]
+    separator_row_parts.extend(
+        [f"{'-' * (length + 4)}" for length in header_lengths[1:]]
+    )
+    return "|".join(separator_row_parts)
+
+
+def _build_data_row_format(
+    reset_color_val,
+    header_names,
+    header_lengths,
+    class_name_col_idx,
+    uncovered_line_numbers_col_idx,
+):
+    return [
+        f"{{color}}{{:<{header_lengths[i]}}}{reset_color_val}"
+        if i == class_name_col_idx or i == uncovered_line_numbers_col_idx
+        else f"{{color}}{{:>{header_lengths[i]}}}{reset_color_val}"
+        for i in range(len(header_names))
+    ]
+
+
+def _build_header_row_formats(header_names, header_lengths):
+    return [
+        f"{{:<{header_lengths[i]}}}" if i == 0 else f"{{:>{header_lengths[i]}}}"
+        for i in range(len(header_names))
+    ]
+
+
+def _calc_class_name_length(coverage_items):
+    return max(
+        [len(ci.class_name) for ci in coverage_items]
+        + [len(ci.class_namespace) for ci in coverage_items]
+    )
+
+
+def _build_namespace_data_row(
+    key: str, items: list[CoverageItem], row_format: str, colorize: bool
+) -> str:
+    ordered_group_column_values = [
+        key,
+        _format_percent(
+            sum(item.covered_lines for item in items),
+            sum(item.coverable_lines for item in items),
+        ),
+        _format_percent(
+            sum(item.covered_branches for item in items),
+            sum(item.branches for item in items),
+        ),
+        "",
+    ]
+
+    color = Fore.GREEN if colorize is True else ""
+    return f"{row_format.format(color=color, *ordered_group_column_values)}\n"
+
+
+def _build_data_row(
+    key: str, item: CoverageItem, row_format: str, colorize: bool
+) -> str:
+    indent = "  " if key != "" else ""
+
+    ordered_column_values = [
+        indent + item.class_name,
+        _format_percent(item.covered_lines, item.coverable_lines),
+        _format_percent(item.covered_branches, item.branches),
+        _compact_number_ranges(item.uncovered_line_numbers),
+    ]
+
+    color = Fore.GREEN if colorize is True else ""
+    return f"{row_format.format(color=color, *ordered_column_values)}\n"
 
 
 def _format_percent(dividend: float, divisor: float) -> str:
