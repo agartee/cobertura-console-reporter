d6855ad19ed2fabe8ca15ee916d4a5a280aea71a
Adam Gartee <agartee@gmail.com>
2023-04-18 14:19:51 -0400

    updated test.ps1 to exit early on test failure

--- scripts/test.ps1
+++ scripts/test.ps1
@@ -5,6 +5,9 @@ Write-Host "Tests:" -ForegroundColor Blue
 Write-Host ""
 
 coverage run -m pytest $testsPath -v --no-header --capture=no
+if ($LASTEXITCODE -ne 0) {
+    exit $LASTEXITCODE
+}
 
 Write-Host ""
 Write-Host "Test Coverage:" -ForegroundColor Blue
