61fad75a60670bf6d5c2ef750c7b5c5bbad71003
Adam Gartee <agartee@gmail.com>
2023-06-20 07:07:25 -0400

    added uncovered branch line number to coverage report parser. appended uncovered lines console output to also include those line numbers.

--- cobertura_console_reporter/parser.py
+++ cobertura_console_reporter/parser.py
@@ -62,15 +62,17 @@ def _create_coverage_item(cls) -> CoverageItem:
     uncovered_line_numbers = []
     branches = 0
     covered_branches = 0
+    uncovered_branch_line_numbers = []
 
     for lines in cls.findall("lines"):
         for line in lines.findall("line"):
             coverable_lines += 1
+            line_number = int(line.get("number"))
 
             if int(line.get("hits")) > 0:
                 covered_lines += 1
             else:
-                uncovered_line_numbers.append(int(line.get("number")))
+                uncovered_line_numbers.append(line_number)
 
             if line.get("branch") == "True":
                 conditional_coverage = _split_conditional_coverage(
@@ -80,6 +82,13 @@ def _create_coverage_item(cls) -> CoverageItem:
                 covered_branches += conditional_coverage[0]
                 branches += conditional_coverage[1]
 
+                for _ in line.findall("conditions/condition"):
+                    if (
+                        int(line_number) not in uncovered_branch_line_numbers
+                        and conditional_coverage[0] < conditional_coverage[1]
+                    ):
+                        uncovered_branch_line_numbers.append(int(line_number))
+
     return CoverageItem(
         class_name,
         file_name,
@@ -88,6 +97,7 @@ def _create_coverage_item(cls) -> CoverageItem:
         uncovered_line_numbers,
         branches,
         covered_branches,
+        uncovered_branch_line_numbers,
     )
 
 
@@ -97,3 +107,4 @@ def _merge_coverage_items(existing: CoverageItem, new: CoverageItem):
     existing.branches += new.branches
     existing.covered_branches += new.covered_branches
     existing.uncovered_line_numbers += new.uncovered_line_numbers
+    existing.uncovered_branch_line_numbers += new.uncovered_branch_line_numbers
